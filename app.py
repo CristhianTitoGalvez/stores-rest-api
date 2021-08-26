153acaae9958d73bc0722022b1b649cbd627c64e
Cristhian Tito <tito_aut12@hotmail.com>
2021-08-26 17:22:06 +0200

    Added PostgreSQL environment variable correction.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-
+import re
+i
 from security import authenticate, identity
 from flask import Flask
 from flask_restful import Api
@@ -11,6 +12,9 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db') #Here we say that the SQLAlchemy data.db is gonna live at the root folder of my project
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # turns off the flask SQLALchemy modification tracker because the library itself come with this feature
 app.secret_key = 'jose'
 api = Api(app)
